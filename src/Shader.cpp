2dedcd6131cbc84abfcb741b1ffe3664921ff360
Wxz234 <429373960@qq.com>
2022-02-24 18:32:01 +0800

    Update Shader.cpp

--- src/Shader.cpp
+++ src/Shader.cpp
@@ -2,14 +2,14 @@
 #include <d3dcompiler.h>
 #include <string>
 namespace RainbowFight {
-	std::wstring GetPath(const wchar_t* path) {
+	std::wstring GetShaderPath(const wchar_t* path) {
 		return std::wstring(RAINBOWFIGHT_PATH) + path;
 	}
 
 	ShaderBlob CompileSkyBoxShader() {
 		ShaderBlob blob;
-		auto vs_path = GetPath(L"/shaders/skybox_vs.hlsl");
-		auto ps_path = GetPath(L"/shaders/skybox_ps.hlsl");
+		auto vs_path = GetShaderPath(L"/shaders/skybox_vs.hlsl");
+		auto ps_path = GetShaderPath(L"/shaders/skybox_ps.hlsl");
 		D3DCompileFromFile(vs_path.c_str(), nullptr, D3D_COMPILE_STANDARD_FILE_INCLUDE, "main", "vs_5_0", 0, 0, &blob.vs, nullptr);
 		D3DCompileFromFile(ps_path.c_str(), nullptr, D3D_COMPILE_STANDARD_FILE_INCLUDE, "main", "ps_5_0", 0, 0, &blob.ps, nullptr);
 
