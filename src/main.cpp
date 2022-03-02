7538b61c2c7c12fb4cd51ad9666c9f10850b5f00
Wxz <429373960@qq.com>
2022-03-03 00:20:42 +0800

    update

--- src/main.cpp
+++ src/main.cpp
@@ -70,7 +70,7 @@ int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, 
     RainbowFight::Camera camera;
     RainbowFight::ImguiInit(&device);
     
-    ShowWindow(hwnd, SW_SHOWDEFAULT);
+    ShowWindow(hwnd, SW_SHOW);
     MSG msg = {};
     while (WM_QUIT != msg.message) {
         if (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE))
